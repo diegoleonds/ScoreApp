d829992b7f8b3c6b467b36ff4a621122589a7927
Diego Leon <diegoleondss@gmail.com>
2020-09-22 21:24:09 -0300

    nome da tela inicial alterado

--- app/src/main/java/com/example/scoreapp/TemporadasActivity.kt
+++ app/src/main/java/com/example/scoreapp/TemporadasActivity.kt
@@ -3,9 +3,9 @@ package com.example.scoreapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class TemporadasActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_temporadas)
     }
 }
