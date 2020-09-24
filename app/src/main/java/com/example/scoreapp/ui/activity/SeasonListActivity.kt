796ed905297ff9b303415c4dbcc31161bfa60431
Diego Leon <diegoleondss@gmail.com>
2020-09-24 18:39:47 -0300

    creating recyclerview adapter class for seasons

--- app/src/main/java/com/example/scoreapp/ui/activity/SeasonListActivity.kt
+++ app/src/main/java/com/example/scoreapp/ui/activity/SeasonListActivity.kt
@@ -2,9 +2,13 @@ package com.example.scoreapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import com.example.scoreapp.R
 
 class SeasonListActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_season_list)
---
+++ app/src/main/java/com/example/scoreapp/ui/adapter/SeasonAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.scoreapp.ui.adapter
+
+class SeasonAdapter {
+}
