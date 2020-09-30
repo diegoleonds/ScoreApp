a28d48cc5bd81b63a5f3d0013ebdd8e25be537d3
Diego Leon <diegoleondss@gmail.com>
2020-09-30 20:47:19 -0300

    Parcelable interface implemented

--- app/src/main/java/com/example/scoreapp/data/model/Game.kt
+++ app/src/main/java/com/example/scoreapp/data/model/Game.kt
@@ -1,5 +1,7 @@
 package com.example.scoreapp.data.model
 
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
@@ -11,10 +13,41 @@ data class Game(
     @ForeignKey(
         entity = Season::class,
         parentColumns = ["id"],
-        childColumns = ["fkSeason"])
+        childColumns = ["fkSeason"]
+    )
     val fkSeason: Long,
     val points: Int,
     val maxRecord: Boolean,
     val minRecord: Boolean
-) {
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readLong(),
+        parcel.readLong(),
+        parcel.readInt(),
+        parcel.readByte() != 0.toByte(),
+        parcel.readByte() != 0.toByte()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeLong(id)
+        parcel.writeLong(fkSeason)
+        parcel.writeInt(points)
+        parcel.writeByte(if (maxRecord) 1 else 0)
+        parcel.writeByte(if (minRecord) 1 else 0)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Game> {
+        override fun createFromParcel(parcel: Parcel): Game {
+            return Game(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Game?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
