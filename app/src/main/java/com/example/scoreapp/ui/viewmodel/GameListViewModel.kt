17ab225d2482f259a3cf6323022cb69421eac038
Diego Leon <diegoleondss@gmail.com>
2020-09-30 20:57:35 -0300

    updating game list and searching for games with max and less points

--- app/src/main/java/com/example/scoreapp/ui/viewmodel/GameListViewModel.kt
+++ app/src/main/java/com/example/scoreapp/ui/viewmodel/GameListViewModel.kt
@@ -2,19 +2,70 @@ package com.example.scoreapp.ui.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.scoreapp.data.model.Game
 import com.example.scoreapp.domain.usecase.CreateGameUseCase
+import com.example.scoreapp.domain.usecase.GetGamesBySeasonUseCase
 import com.example.scoreapp.ui.model.Season
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import org.koin.core.KoinComponent
-import org.koin.core.inject
 
 class GameListViewModel : ViewModel() {
     val createGameUseCase = CreateGameUseCase()
+    val getGamesUseCase = GetGamesBySeasonUseCase()
     val season: MutableLiveData<Season> = MutableLiveData()
+    val games: MutableLiveData<ArrayList<Game>> = MutableLiveData()
+
+    suspend fun updateGamesList() {
+        season.value?.let {
+            games.postValue(ArrayList(getGamesUseCase.getGamesBySeason(it.id)))
+            updateGamesOfCurrentSeason()
+        }
+    }
 
     suspend fun addGame(points: Int) {
         createGameUseCase.createGame(points, season.value)
+        updateGamesList()
+    }
+
+    fun getGameWithMaxRecord(): Int? {
+        games.value?.let {
+            if (it.size < 2){
+                return null
+            }
+            var gamePosition = 0
+            for (i in 1 until it.size) {
+                if (it.get(i).points > it.get(gamePosition).points)
+                    gamePosition = i
+            }
+            return gamePosition
+        }
+        return null
+    }
+
+    fun getGameWithMinRecord(): Int? {
+        games.value?.let {
+            if (it.size < 2){
+                return null
+            }
+            var gamePosition = 0
+            for (i in 1 until it.size) {
+                if (it.get(i).points < it.get(gamePosition).points)
+                    gamePosition = i
+            }
+            return gamePosition
+        }
+        return null
+    }
+
+    fun updateGamesOfCurrentSeason() {
+        games.value?.let {
+            season.value?.run {
+                for (game: Game in it) {
+                    if (game.points > this.maxRecord) {
+                        this.maxRecord = game.points
+                    } else if (game.points < this.minRecord) {
+                        this.minRecord = game.points
+                    }
+                }
+            }
+        }
     }
 }
