5303ce5ae64885f4db62e14eeb9104ecf866d241
Diego Leon <diegoleondss@gmail.com>
2020-10-02 23:23:36 -0300

    new viewmodels

--- app/src/main/java/com/example/scoreapp/ui/di/UiModules.kt
+++ app/src/main/java/com/example/scoreapp/ui/di/UiModules.kt
@@ -1,9 +1,12 @@
 package com.example.scoreapp.ui.di
 
 import android.content.Intent
+import com.example.scoreapp.domain.usecase.UpdateGameUseCase
 import com.example.scoreapp.ui.activity.GameListActivity
 import com.example.scoreapp.ui.model.Season
 import com.example.scoreapp.ui.viewmodel.GameListViewModel
+import com.example.scoreapp.ui.viewmodel.GameViewModel
+import com.example.scoreapp.ui.viewmodel.SeasonInfoViewModel
 import com.example.scoreapp.ui.viewmodel.SeasonListViewModel
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -11,4 +14,6 @@ import org.koin.dsl.module
 val viewModelModule = module {
     viewModel { SeasonListViewModel(get(), get(), get()) }
     viewModel { GameListViewModel(get(), get(), get()) }
+    viewModel { SeasonInfoViewModel(get()) }
+    viewModel { GameViewModel(get(), get()) }
 }
