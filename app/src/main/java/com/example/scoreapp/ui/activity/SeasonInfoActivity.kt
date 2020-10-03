1ba6355bd7fccc199f48f9628f012a29937ae895
Diego Leon <diegoleondss@gmail.com>
2020-10-02 23:22:17 -0300

    displayng the correct data

--- app/src/main/java/com/example/scoreapp/ui/activity/SeasonInfoActivity.kt
+++ app/src/main/java/com/example/scoreapp/ui/activity/SeasonInfoActivity.kt
@@ -2,22 +2,58 @@ package com.example.scoreapp.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import com.example.scoreapp.R
 import com.example.scoreapp.ui.model.Season
+import com.example.scoreapp.ui.viewmodel.SeasonInfoViewModel
 import kotlinx.android.synthetic.main.activity_season_info.*
 import kotlinx.android.synthetic.main.cardview_points.*
 import kotlinx.android.synthetic.main.cardview_record_info.view.*
 import kotlinx.android.synthetic.main.toolbar_back_icon.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.koin.android.viewmodel.ext.android.viewModel
 
-class SeasonInfoActivity(): AppCompatActivity() {
+class SeasonInfoActivity() : AppCompatActivity() {
+
+    val viewModel: SeasonInfoViewModel by viewModel()
+    lateinit var season: Season
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_season_info)
 
-        val season = intent.getParcelableExtra<Season>(getString(R.string.parcelable_season))
+        season = intent.getParcelableExtra<Season>(getString(R.string.parcelable_season))
+        backToolbarButton.setOnClickListener { finish() }
+        populateViews()
+        observeViewModelData()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        updateViewModelData()
+    }
+
+    fun observeViewModelData() {
+        viewModel.games.observe(this, Observer {
+            pointsTextView.text = "${viewModel.getSeasonAverageScore().toString()} points"
+            includeSeasonFrequency.seasonRecordPointsTextView.text =
+                "${viewModel.getMinRecordFrequency()} - ${viewModel.getMaxRecordFrequency()}"
+        })
+    }
+
+    fun updateViewModelData(){
+        CoroutineScope(Dispatchers.IO).launch {
+            viewModel.getSeasonGames(season.id)
+        }
+    }
+
+    fun populateViews(){
         backToolbarTitle.text = getString(R.string.season_info_activity_title)
-        includeSeasonPoints.recordInfoTitleTextView.text = getString(R.string.record_points_cardview_title)
+        includeSeasonPoints.recordInfoTitleTextView.text =
+            getString(R.string.record_points_cardview_title)
+
         includeSeasonPoints.seasonRecordPointsTextView.text =
             "${season.minRecord} - ${season.maxRecord}"
 
@@ -25,7 +61,5 @@ class SeasonInfoActivity(): AppCompatActivity() {
             getString(R.string.record_frequency_cardview_title)
 
         pointsTitleTextView.text = getString(R.string.average_score_cardview_title)
-
-        backToolbarButton.setOnClickListener { finish() }
     }
 }
